1a986beccda024865c7db1f9b9916ce2c1607057
Angelicarjs <37352302+Angelicarjs@users.noreply.github.com>
2018-04-15 16:51:40 -0500

    Update suave.py
    Subida de imagen OK
    Cambio de imagen a blanco/negro  y subida de esa nueva matriz OK
    Transformada de fourier 2d con errores en la aplicacion
    Primeros avances en el calculo de la gaussiana en 2d

--- punto_1/suave.py
+++ punto_1/suave.py
@@ -1,34 +1,72 @@
 import numpy as np
 import matplotlib.pylab as plt 
-from scipy.fftpack import fft,fftfreq
+from PIL import Image, ImageOps
 
-#subir la imagen como una matriz
-imagenc = plt.imread('jh.png')
-plt.figure(1, figsize=(9.5,9))
-plt.imshow(imagenc)
-#plt.show()
+#subir la imagen como una matriz (se debe poner entre comillas la ubicacion de la imagen)
+def sube_imagen(dire):
+	return Image.open(str(dire))
+
+
+color = sube_imagen('ult.png')
 
 #Cambio de matriz de 3d (color) a matriz 2d (blanco y negro) 
-imagenbyn = imagenc[:,:,0]
-plt.imshow(imagenbyn)
-plt.show()
+def byn(imag):
+	b_n= ImageOps.grayscale(imag)
+	b_n.save('ima.png')
+	#Subo nueva imagen a blanco y negro para obtener el array
+	imab_n = plt.imread('ima.png')
 
-#Transformada de Fourier en dos dimensiones aplicada a la matriz de la imagen 
-def transformada(matriz):
-	for n in range(0,N):	
-		expo = (-1j)*(2.0)*(3.1416)*m*(float(n)/float(N))
-		tran = np.sum(y*np.exp(expo))
-		listaf.append(tran)
+	return imab_n
+
+byne = byn(color)
+
+
+#Transformada de Fourier en dos dimensiones  
+#Parametro m: matriz de la imagen en blanco y negro o matriz de la gaussiana 
+def transformada(mtz):
+	#Dimensiones de la matriz de la imagen 
+	(M,N) = np.shape(mtz)
+	
+	#Matriz donde se guarda la transformada 
+	mtrans = np.zeros((M,N), dtype = complex) 
+
+	k = np.linspace(0.0, N-1, N)
+	j = np.linspace(0.0, M-1, M)
+	for m in range(M):
+		for n in range (N):
+			a = k*(float(n)/float(N))
+			b = j*(float(m)/float(M))
+			expo = (-1j)*(2.0)*(3.1416)*(float(a+b))
+			tran_act = np.sum(mtz[m,n]*np.exp(expo))
+
+		mtrans[m,n] = tran_act
+
+	return mtrans
+
 #Gaussiana en dos dimensiones
+def transgaus(m):
 
-#Aplicación de la transformada a la gaussiana 
+	#Dimensiones de la matriz de la imagen 
+	(M,N) = np.shape(m)
+
+	#Da los centros de la imagen que utilizare en la gaussiana y los guarda en variables globales
+	if(M%2 == 0):
+		yO = M/2
+	else: 
+		yO = (M/2)+1
+ 
+	if(N%2 == 0):
+		xO = N/2
+
+	else: 
+		xO = (N/2)+1
+ 
+	
+#Aplicacion de la transformada a la gaussiana 
 
 #Convolucion entre las transformadas (gaussiana e imagen)
 
 #Aplicacion de la transformada inversa al resultado de lo anterior 
-
-
-
 
 
 
