35d9b9b2778af3077d8ba2d1c3f4f7a6c3792d21
Angelicarjs <37352302+Angelicarjs@users.noreply.github.com>
2018-04-16 16:46:49 -0500

    Update suave.py
    Resultado final

--- punto_1/suave.py
+++ punto_1/suave.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pylab as plt 
+import sys 
 from PIL import Image, ImageOps 
+import cmath
+
+#sigma(ancho de la gaussiana) y nombre de la imagen a la que se le quiere aplicar ingresada por el usuario
+sig = sys.argv[2]
+nombre = sys.argv[1]
 
 #subir la imagen como una matriz blanco y negro (se debe poner entre comillas la ubicacion de la imagen)
 def sube_imagen(dire):
-	return Image.open(dire)
-
-color = sube_imagen('jhh.png')
-
-#Cambio de matriz de 3d (color) a matriz 2d (blanco y negro) 
-def byn(imag):
-	b_n= ImageOps.grayscale(imag)
+	cargo = Image.open(dire)
+	b_n = cargo.convert('L')
 	b_n.save('ima.png')
 	#Subo nueva imagen a blanco y negro para obtener el array
 	imab_n = plt.imread('ima.png')
 	return imab_n
 
-byne = byn(color)
-
 #Transformada de Fourier en dos dimensiones  
 #Parametro m: matriz de la imagen en blanco y negro o matriz de la gaussiana 
 def transformada(mtz):
+
 	#Dimensiones de la matriz de la imagen 
 	(M,N) = np.shape(mtz)
 	
@@ -36,15 +36,12 @@ def transformada(mtz):
 					expo = (-1j)*(2.0)*(3.1416)*(float(a+b))
 					tran_act += mtz[m,n]*np.exp(expo)
 			
-			mtrans[i,j] = tran_act
+			mtrans[i,j] = tran_act/N/M
 	return mtrans
-
-#Aplicacion de la transformada a la imagen 
-transim = transformada(byne)
-
-
+	
+	
 #Gaussiana en dos dimensiones generada a partir de la matriz de la imagen que entra como parametro
-def gaussiana(mtz):
+def gaussiana(mtz, sigma):
 
 	#Dimensiones de la matriz de la imagen 
 	(M,N) = np.shape(mtz)
@@ -66,19 +63,42 @@ def gaussiana(mtz):
 
 	else: 
 		xO = (N/2)+1
- 	
-	#ancho de la gaussiana 
-	sigma = 0.3
-	
-	A = 1.0 / (np.sqrt(2.0*3.1416*sigma*sigma))
+ 		
+	A = 1.0 / (np.sqrt(2.0*3.1416*float(sigma)**2))
 	for y in range(M):
 		for x in range(N):
-			gx = float((x-xO)**2)/2.0*sigma*sigma
-			gy = float((y-yO)**2)/2.0*sigma*sigma
+			gx = float((x-xO)**2)/(2.0*float(sigma)**2)
+			gy = float((y-yO)**2)/(2.0*float(sigma)**2)
 			mgauss[y,x] = A*(np.exp(-(gx+gy)))
 	return mgauss
 
-ga = gaussiana(byne)
+#Funcion que retorna la inversa de la transformada de Fourier
+def invtra(mtz):
+	#Dimensiones de la matriz de la imagen 
+	(M,N) = np.shape(mtz)
+
+	mtrans = np.zeros((M,N), dtype= float)
+	for m in range(M):
+		for n in range(N):
+			tran_act = 0.0
+			for i in range(M):
+				for j in range(N):
+					a = float(i*m)/float(M) 
+					b = float(j*n)/float(N)
+					expo = 1j*2.0*3.1416*(a+b)
+					tran_act += mtz[j][i] * np.exp(expo)
+			mtrans[m,n]=(tran_act.real)
+	return (mtrans)
+
+
+#Sube imagen que ingreso el usuario 
+byne = sube_imagen(nombre)
+
+#Aplicacion de la transformada a la imagen 
+transim = transformada(byne)
+
+#Aplicacion de la gaussiana a la matriz de la imagen 
+ga = gaussiana(byne,sig)
 
 #Aplicacion de la transformada a la gaussiana 
 transgau= transformada(ga)
@@ -86,32 +106,11 @@ transgau= transformada(ga)
 #Convolucion entre las transformadas (gaussiana e imagen)
 con= transgau*transim
 
-#Funcion que retorna la inversa de la transformada de Fourier
-def invtra(mtz):
-	#Dimensiones de la matriz de la imagen 
-	(M,N) = np.shape(mtz)
-	
-	#Matriz donde se guarda la transformada 
-	mtrans = np.zeros((M,N), dtype = float) 
-	for m in range(M):
-		for n in range(N):
-			tran_act = 0.0
-			for i in range(M):
-				for j in range (N):
-					a = i*(float(n)/float(N))
-					b = j*(float(m)/float(M))
-					expo = (1j)*(2.0)*(3.1416)*(float(a+b))
-					tran_act += mtz[m,n]*np.exp(expo)
-			
-			mtrans[m,n] = tran_act
-	return mtrans
-
 #Aplicacion de la transformada inversa al resultado de lo anterior entra como parametro la matriz de la convoluncion anteriormente realizada 
 
 rta = invtra(con)
 
-#plt.figure()
-#plt.imread(rta)
-#plt.show()
-#plt.save("suave.png")
+plt.figure()
+plt.imshow(rta, cmap="gray")
+plt.save("suave.png")
 
